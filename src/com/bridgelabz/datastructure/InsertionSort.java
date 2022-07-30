78a45729cab1dbbd9c340749bfa4e059752026b2
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-30 15:13:17 +0530

    alogorithm problems

--- src/com/bridgelabz/datastructure/InsertionSort.java
+++ src/com/bridgelabz/datastructure/InsertionSort.java
@@ -4,21 +4,22 @@ import java.util.Arrays;
 
 public class InsertionSort {
 
-    public void insertionSort(int[] array) {
+    public void insertionSort(String[] array) {
 
         for (int i = 0; i < (array.length-1); i++) {
-            int num = array[i+1];
+            String str = array[i+1];
 
-            while (num < array[i]) {
+
+            while (i>=0 && ((str.compareTo(array[i])) < 0)) {
                 array[i + 1] = array[i];
                 i--;
             }
-            array[i + 1] = num;
+            array[i + 1] = str;
         }
     }
 
     public static void main(String[] args) {
-        int[] data = { 2, 6, 4, 9, 8, 5 };
+        String[] data = { "Nive","Abhi","Sagar","Shreyas","Megha" };
         InsertionSort insert = new InsertionSort();
         insert.insertionSort(data);
         System.out.println("Sorted Array : ");
